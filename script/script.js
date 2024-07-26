68917729c233d9d977e250ce2201954127e31a6c
Arunsamuvel <arun15ee03@gmail.com>
2024-07-26 19:43:09 +0530

    completed the Digital Clock project

--- script/script.js
+++ script/script.js
@@ -1,13 +1,28 @@
 "use strict";
+// select the inputs
 let hrs = document.getElementById("hrs");
 let mins = document.getElementById("mins");
 let sec = document.getElementById("sec");
+let date = document.getElementById("date");
+let month = document.getElementById("month");
+let year = document.getElementById("year");
 
+// Asynchronous function setinterval is used to continue running my code
 setInterval(() => {
+  // create varibles called new data this is inbuild function date()
+
   let currentTime = new Date();
+  console.log(currentTime);
+  //   using get method to get only the data and stored to my html element
+
   hrs.innerHTML = currentTime.getHours();
   mins.innerHTML = currentTime.getMinutes();
   sec.innerHTML = currentTime.getSeconds();
+  date.innerHTML = currentTime.getDate();
+  month.innerHTML = currentTime.getUTCMonth();
+  year.innerHTML = currentTime.getFullYear();
+
+  //  if else statement are used to correct the 0 s in html format
 
   if (currentTime.getHours() < 10) {
     hrs.innerHTML = "0" + currentTime.getHours();
